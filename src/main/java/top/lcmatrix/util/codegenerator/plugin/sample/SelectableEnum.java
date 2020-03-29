581e3575c5502d29ac20bbb55b04ef52e9bcd385
penghaifeng <penghf1@lenovo.com>
2020-03-29 23:36:41 +0800

    优化示例

--- src/main/java/top/lcmatrix/util/codegenerator/plugin/sample/SelectableEnum.java
+++ src/main/java/top/lcmatrix/util/codegenerator/plugin/sample/SelectableEnum.java
@@ -3,7 +3,7 @@ package top.lcmatrix.util.codegenerator.plugin.sample;
 import top.lcmatrix.util.codegenerator.common.plugin.ISelectableEnum;
 
 public enum SelectableEnum implements ISelectableEnum {
-    DOG("dog"), CAT("cat"), PIG("pig");
+    DOG("dog"), CAT("cat"), PIG("pig"), COW("cow"), SHEEP("sheep"), HORSE("horse");
 
     private String displayText;
     SelectableEnum(String displayText){
